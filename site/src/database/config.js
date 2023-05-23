e608089db52c7b4a289153674df0043dbc838543
jldl <matheus.santiago@sptech.school>
2023-05-23 13:45:23 -0300

    alteracoes api

--- site/src/database/config.js
+++ site/src/database/config.js
@@ -21,8 +21,8 @@ var sqlServerConfig = {
 var mySqlConfig = {
     host: "localhost",
     database: "rivus",
-    user: "Rivus",
-    password: "rivus123",
+    user: "aluno",
+    password: "sptech",
 };
 
 function executar(instrucao) {
--- site/src/models/usuarioModel.js
+++ site/src/models/usuarioModel.js
@@ -12,7 +12,7 @@ function listar() {
 function entrar(email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
     var instrucao = `
-        SELECT * FROM empresa WHERE email = '${email}' AND senha = '${senha}';
+        SELECT * FROM cadastroEmpresa WHERE email = '${email}' AND senha = '${senha}';
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
@@ -25,7 +25,7 @@ function cadastrar(empresa, cnpj, email, senha) {
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucao = `
-        INSERT INTO empresa (nomeEmpresa, cnpj, email, senha) VALUES ('${empresa}', '${cnpj}', '${email}', '${senha}');
+        INSERT INTO cadastroEmpresa (nomeEmpresa, cnpj, email, senha) VALUES ('${empresa}', '${cnpj}', '${email}', '${senha}');
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
