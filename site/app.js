4ae651482bfd276fb15fc1180108792d1f70534c
Daniel Rodrigues <this.daniel@outlook.com>
2023-05-27 14:54:19 -0300

    rebase configs app.js

--- site/app.js
+++ site/app.js
@@ -1,20 +1,10 @@
 process.env.AMBIENTE_PROCESSO = "desenvolvimento";
 // process.env.AMBIENTE_PROCESSO = "producao";
 
-const serialport = require('serialport');
 var express = require("express");
-const mysql = require('mysql2');
-const sql = require('mssql');
 var cors = require("cors");
 var path = require("path");
-//var PORTA = process.env.AMBIENTE_PROCESSO == "desenvolvimento" ? 3333 : 8080;
-// não altere!
-const SERIAL_BAUD_RATE = 9600;
-const SERVIDOR_PORTA = 3300;
-// altere o valor da variável AMBIENTE para o valor desejado:
-// API conectada ao banco de dados remoto, SQL Server -> 'producao'
-// API conectada ao banco de dados local, MySQL Workbench - 'desenvolvimento'
-const AMBIENTE = 'desenvolvimento';
+var PORTA = process.env.AMBIENTE_PROCESSO == "desenvolvimento" ? 3333 : 8080;
 
 var app = express();
 
@@ -34,15 +24,10 @@ app.use("/usuarios", usuarioRouter);
 app.use("/avisos", avisosRouter);
 app.use("/medidas", medidasRouter)
 
-app.listen(SERVIDOR_PORTA, function () {
+app.listen(PORTA, function () {
     console.log(`Servidor do seu site já está rodando! Acesse o caminho a seguir para visualizar: http://localhost:${PORTA} \n
     Você está rodando sua aplicação em Ambiente de ${process.env.AMBIENTE_PROCESSO} \n
     \t\tSe "desenvolvimento", você está se conectando ao banco LOCAL (MySQL Workbench). \n
     \t\tSe "producao", você está se conectando ao banco REMOTO (SQL Server em nuvem Azure) \n
     \t\t\t\tPara alterar o ambiente, comente ou descomente as linhas 1 ou 2 no arquivo 'app.js'`);
 });
-
-// configure a linha abaixo caso queira que os dados capturados sejam inseridos no banco de dados.
-// false -> nao insere
-// true -> insere
-const HABILITAR_OPERACAO_INSERIR = true;
