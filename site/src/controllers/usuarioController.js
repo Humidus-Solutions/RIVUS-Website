39accbd218c8abc81227eb7097c4bb7bd76d2af8
VictorBeralde <victor.cardoso@sptech.school>
2023-05-30 14:40:47 -0300

    Ligação com a controller

--- site/src/controllers/usuarioController.js
+++ site/src/controllers/usuarioController.js
@@ -116,9 +116,53 @@ function cadastrar(req, res) {
     }
 }
 
+function cadastrarLitros(req, res) {
+    // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
+    var litros = req.body.litrosServer;
+    var idEmpresa = 1;
+
+    // Faça as validações dos valores
+    if (litros == undefined) {
+        res.status(400).send("Os litros estão undefined!");
+    } else {
+        
+        // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
+        usuarioModel.cadastrarLitros(litros, idEmpresa)
+            .then(
+                function (resultado) {
+                    res.json(resultado);
+                }
+            ).catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log(
+                        "\nHouve um erro ao realizar o cadastro! Erro: ",
+                        erro.sqlMessage
+                    );
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+
+        usuarioModel.cadastrarLitros(litros, idEmp).then(
+            function(response) {
+                res.json(response);
+            }
+        ).catch(
+            function (error) {
+                console.log(error)
+                console.log(
+                    "\nHouve um erro ao realizar o cadastro! Erro: ",
+                        error.sqlMessage
+                );
+                res.status(500).json(error.sqlMessage);
+            }
+        )
+    }
+}
 module.exports = {
     entrar,
     cadastrar,
+    cadastrarLitros,
     listar,
     testar
 }
